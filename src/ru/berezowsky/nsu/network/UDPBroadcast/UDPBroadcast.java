ac28419972040ab5aaf89de7affdff4c9d4893f2
Nicolay Berezowsky <nicolay@berezowsky.ru>
2016-09-28 17:10:42 +0700

    sent UDP broadcast message

--- src/ru/berezowsky/nsu/network/UDPBroadcast/UDPBroadcast.java
+++ src/ru/berezowsky/nsu/network/UDPBroadcast/UDPBroadcast.java
@@ -1,5 +1,25 @@
 package ru.berezowsky.nsu.network.UDPBroadcast;
 
-public class UDPBroadcast {
+import java.io.IOException;
+import java.net.*;
 
+public class UDPBroadcast {
+    public static void main(String[] args) {
+        int port = 5555;
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+
+            socket.setBroadcast(true);
+
+            InetAddress address = InetAddress.getByName("192.168.0.255");
+            DatagramPacket packet = new DatagramPacket("helo".getBytes(), 4, address, 5555);
+            socket.send(packet);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
